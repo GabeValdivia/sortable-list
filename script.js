2f3b9257bdb21e6b4f7ff9263b98f078460a353b
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-15 21:10:25 -0600

    Write needed variables to getElementById, hard code array of richestPeople, create an empty array to stor list items

---
+++ script.js
@@ -0,0 +1,18 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+	'Jeff Bezos',
+	'Bill Gates',
+	'Warren Buffet',
+	'Bernard Arnault',
+	'Carlos Slim Helu',
+	'Amancio Ortega',
+	'Larry Ellison',
+	'Mark Zuckerberg',
+	'Michael Bloomberg',
+	'Larry Page'
+]
+
+// Store list items
+const listItems = [];
